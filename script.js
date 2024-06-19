7e8dceef86743173da1d597a912df8585c902f7b
johnatanf <johnatan.f100@gmail.com>
2024-06-19 10:53:50 +0700

    add populateFields and createTypesHTML functions

--- script.js
+++ script.js
@@ -33,3 +33,83 @@ function clearFields() {
         currentEl.innerHTML = ""
     }
 }
+
+function createTypesHTML(arr) {
+    let finalHTML = ""
+    
+    for(item of arr) {
+        finalHTML = finalHTML.concat(`<li class="type">${item}</li>`)
+    }
+
+    return finalHTML
+}
+
+async function populateFields(searchString) {
+    const data = await retrieveData(searchString)
+    clearFields()
+
+    if(!data.error) {
+        let pokemonName = data.name
+        let pokemonId = data.id
+        let weight = data.weight
+        let height = data.height
+        let types = data.types.map(item => item.type.name)
+        let hp
+        let attack
+        let defense
+        let specialAttack
+        let specialDefense
+        let speed
+
+        const pokemonNameField = document.getElementById('pokemon-name')
+        const pokemonIdField = document.getElementById('pokemon-id')
+        const weightField = document.getElementById('weight')
+        const heightField = document.getElementById('height')
+        const typesField = document.getElementById('types')
+        const hpField = document.getElementById('hp')
+        const attackField = document.getElementById('attack')
+        const defenseField = document.getElementById('defense')
+        const specialAttackField = document.getElementById('special-attack')
+        const specialDefenseField = document.getElementById('special-defense')
+        const speedField = document.getElementById('speed')
+
+        for(item of data.stats) {
+            switch(item.stat.name) {
+                case 'attack':
+                    attack = item.base_stat
+                    break
+                case 'hp':
+                    hp = item.base_stat
+                    break
+                case 'defense':
+                    defense = item.base_stat
+                    break
+                case 'special-attack':
+                    specialAttack = item.base_stat
+                    break
+                case 'special-defense':
+                    specialDefense = item.base_stat
+                    break
+                case 'speed':
+                    speed = item.base_stat
+                    break
+                default:
+            }
+        }
+
+        pokemonNameField.innerHTML = pokemonName
+        pokemonIdField.innerHTML = pokemonId
+        weightField.innerHTML = weight
+        heightField.innerHTML = height
+        typesField.innerHTML = createTypesHTML(types)
+        hpField.innerHTML = hp
+        attackField.innerHTML = attack
+        defenseField.innerHTML = defense
+        specialAttackField.innerHTML = specialAttack
+        specialDefenseField.innerHTML = specialDefense
+        speedField.innerHTML = speed
+        
+    } else {
+        alert("Pokémon not found")
+    }
+}
